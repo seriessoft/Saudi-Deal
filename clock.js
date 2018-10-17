557f25e3d2057d0826ede721725aff39e855c966
seriessoft <khaleelbasuny@yahoo.com>
2018-10-17 18:09:14 +0300

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,6 +1,6 @@
 
 var CronJob = require('cron').CronJob;
-var bot = require('./bot.js');
+var bot = require('./worker.js');
 
 var job = new CronJob({
   cronTime: "13 9,11,16,20,22 * * *", // everyday, 9:13, 11:13, 4:13, 8:13,
