a69cf1b77f14e0525bb50730e54a3ca7a2d7ebfe
seriessoft <khaleelbasuny@yahoo.com>
2018-10-17 18:06:42 +0300

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,1 +1,12 @@
 
+var CronJob = require('cron').CronJob;
+var bot = require('./bot.js');
+
+var job = new CronJob({
+  cronTime: "13 9,11,16,20,22 * * *", // everyday, 9:13, 11:13, 4:13, 8:13,
+  onTick: bot.start(),
+  start: true,
+  timeZone: "America/Los_Angeles"
+});
+
+job.start();
