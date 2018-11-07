5630bb8816947bb171e44cf0a6ee232b2064d63b
seriessoft <khaleelbasuny@yahoo.com>
2018-11-07 15:36:14 +0300

    Update ranks.js

--- utils/ranks.js
+++ utils/ranks.js
@@ -4,6 +4,8 @@ var firebase = require('firebase');
 var evaluatePromotion = function(userId){
   var player = Firebase.database().ref().child('users').child(userId);
   player.once('value').then(function(snap){
+    var playerRank = snap.val().child('rank');
+    var rankData = Firebase.database().ref().child('rank').child(rankData.myRank);
     if (winStreak => winPromo){
       if(wallet => promoFees){
        //updateSubRank
