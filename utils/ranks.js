122e64a66aba2ca83b361abf2f9b853410705466
seriessoft <khaleelbasuny@yahoo.com>
2018-11-07 15:14:11 +0300

    Create ranks.js

---
+++ utils/ranks.js
@@ -0,0 +1,21 @@
+var firebase = require('firebase;);
+
+
+var evaluatePromotion = function(){
+  if (winStreak => winPromo){
+    if(wallet => promoFees){
+      //updateSubRank
+      //updateRank
+    }
+  }
+};
+
+var evaluateDemotion = function(){
+  if(rank => 1)
+    if(lossStreak => lossDemo){
+      //updateSubRank
+      //updateRank
+    }
+  }
+};
+
