91aba371a713f1190db40e6c02c70925183bc740
seriessoft <khaleelbasuny@yahoo.com>
2018-11-07 15:29:17 +0300

    Update ranks.js

--- utils/ranks.js
+++ utils/ranks.js
@@ -1,22 +1,28 @@
 var firebase = require('firebase');
 
 
-var evaluatePromotion = function(){
-  if (winStreak => winPromo){
-    if(wallet => promoFees){
-      //updateSubRank
-      //updateRank
+var evaluatePromotion = function(userId){
+  var player = Firebase.database().ref().child('users').child(userId);
+  player.once('value').then(function(snap){
+    if (winStreak => winPromo){
+      if(wallet => promoFees){
+       //updateSubRank
+       //updateRank
+      }
     }
-  }
+  };
 };
 
-var evaluateDemotion = function(){
-  if(rank => 1)
-    if(lossStreak => lossDemo){
-      //updateSubRank
-      //updateRank
+var evaluateDemotion = function(userId){
+  var player = Firebase.database().ref().child('users').child(userId);
+  player.once('value').then(function(snap){
+    if(rank => 1)
+      if(lossStreak => lossDemo){
+        //updateSubRank
+        //updateRank
+      }
     }
-  }
+  };
 };
 
 module.exports = {
